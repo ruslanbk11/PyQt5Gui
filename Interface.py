285bd740a132472f00aecd7055652893674a88d6
Ruslan <ruslanbk11@mail.ru>
2018-03-08 13:45:34 +0300

    Removed QLabel, added QGraphicsView

--- Interface.py
+++ Interface.py
@@ -15,19 +15,20 @@ class Window(QtWidgets.QMainWindow):
 
         self.ui.actionLoad.triggered.connect(self.openFile)
         self.ui.actionSave.triggered.connect(self.saveFile)
-        self.ui.label.setText("")
+        #self.ui.label.setText("")
         
     def openFile(self):
         options = QtWidgets.QFileDialog.Options()
         self.file,_=QtWidgets.QFileDialog.getOpenFileName(None,'OpenFile','',
                                                            "Image file(*.png *.jpg)",
                                                            options=options)
-        self.image = QtGui.QImage(self.file)
-        #painter = QtGui.QPainter()
-        #painter.begin(self)
-        #painter.drawImage(0, 0, image)
-        #painter.end()
-        self.ui.label.setPixmap(QtGui.QPixmap.fromImage(self.image))
+        self.image = QtGui.QPixmap(self.file)
+        #self.ui.label.setPixmap(self.image)
+        self.resize(self.image.size())
+        self.ui.scene = QtWidgets.QGraphicsScene()
+        self.ui.scene.addPixmap(self.image)
+        self.ui.graphicsView.setScene(self.ui.scene)
+        #self.ui.graphicsView.resize(self.image.width() + 2, self.image.height() + 2)
         self.closeCheck = True
 
     def saveFile(self):
@@ -36,15 +37,23 @@ class Window(QtWidgets.QMainWindow):
                                               "", "Image file(*.png *.jpg)",
                                               options=options)
         if self.fileName:
-            fileFormat = self.fileName[-3] + self.fileName[-2] + self.fileName[-1]
-            self.pixmap = self.ui.label.pixmap()
-            self.image = self.pixmap.toImage()
-            if self.image.save(self.fileName, fileFormat):
+            #fileFormat = self.fileName[-3] + self.fileName[-2] + self.fileName[-1]
+            #self.pixmap = self.ui.label.pixmap()
+            #self.image = self.pixmap.toImage()
+            #if self.image.save(self.fileName, fileFormat):
+            #    self.ui.statusbar.showMessage('Saved to %s' %self.fileName)
+            #    self.closeCheck = False
+            self.image = QtGui.QImage(self.ui.scene.sceneRect().size().toSize(),
+                                      QtGui.QImage.Format_ARGB32)
+            self.image.fill(QtGui.QColor.fromRgb(255, 255, 255, 255));
+            painter = QtGui.QPainter(self.image)
+            self.ui.scene.render(painter)
+            if self.image.save(self.fileName):
                 self.ui.statusbar.showMessage('Saved to %s' %self.fileName)
                 self.closeCheck = False
             else:
                 self.ui.statusbar.showMessage('!!! File is NOT saved !!!')
-     
+
     def closeEvent(self, event):
         if self.closeCheck:
             result = QtWidgets.QMessageBox.question(self, "Confirm action",
